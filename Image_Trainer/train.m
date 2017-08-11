4203d09ef546a551405c412a0ff71f35fc3c1187
hleou <hleou@stanford.edu>
2017-08-11 13:15:47 -0700

    Updated Image Trainer

--- Image_Trainer/train.m
+++ Image_Trainer/train.m
@@ -32,47 +32,41 @@ for n = 1:n_samples
     global pst_x = zeros(1,n_points);
     global pst_y = zeros(1,n_points);
     occluded = zeros(1,n_points);
-    for i = 1:5
-        disp('Select 1 point in the middle of each finger (Begin at pinky finger)')
-        [x, y] = getpts(1);
-        disp('Point Selected: (' + x(1) + ',' + y(1) + ')');
-        pst_x(i) = x(1); pst_y(i) = y(1);
-        pst_x(i+1) = x(1); pst_y(i+1) = y(1);
-    end
-    for i = 1:5
-        disp('Select 1 point on each fingertip (Begin at pinky finger)');
-        [x, y] = getpts(1);
-        disp('Point Selected: (' + x(1) + ',' + y(1) + ')');
-        pst_x(i) = x(1); pst_y(i) = y(1);
-        pst_x(i+1) = x(1); pst_y(i+1) = y(1);
-    end
-end
-
-function point_collect(double_record)
-    [x, y] = getpts(1);
-    disp('Point Selected: (' + x(1) + ',' + y(1) + ')');
-    if(double_record)
-        
-    end 
-        
-        
-end 
-
+    disp('Select 1 point in the middle of each finger (Begin at pinky finger)');
+    point_collect((1:5), TRUE);
+    disp('Select 1 point on each fingertip (Begin at pinky finger)');
+    point_collect((11:15), FALSE);
+    disp('Select 1 point on each edge (median height) of the hand (Left/Right)');
+    point_collect((16:17), FALSE);
+    disp('Select the joint between the (1)Pinky/Ring Fingers and (2)Thumb/Index Fingers');
+    point_collect((18:19), TRUE);
+    disp('Select the outside (1)Pinky base and (2)Thumb base');
+    point_collect((22:23), TRUE);
+    disp('Select 3 landmarks on the wrist (Left, Center, Right)');
+    point_collect((26:28), FALSE);
+    disp('Select 1 landmarks in the center of the palm');
+    point_collect((29:29), FALSE);
     
-    
-    
-    phis_el = [pst_x, pst_y, occluded];
-    phisTr = vertcat(phisTr, phis_el);
+    disp('Done Collecting Landmarks');
+    phisTr = vertcat(phisTr, [pst_x, pst_y, occluded];
     
     %____Adding ROI(Rectangle) to bboxesTr____%
-    disp("rectangle")
+    disp('Select a Rectangular ROI');
     rect = getrect;
     bboxesTr = vertcat(bboxesTr, rect);
 end
+    save('DATA_T.mat', 'IsT', 'phisT', 'bboxesT')
+    save('DATA_Tr.mat', 'IsTr', 'phisTr', 'bboxesTr')
 
-IsT = IsTr;
-phisT = phisTr;
-bboxesT = bboxesTr;
-
-save('DATA_T.mat', 'IsT', 'phisT', 'bboxesT')
-% save('DATA_Tr.mat', 'IsTr', 'phisTr', 'bboxesTr')
+function point_collect(range, double_record)
+for i=range
+    [x, y] = getpts(1);
+    disp('Point Selected: (' + x(1) + ',' + y(1) + ')');
+    if(double_record)
+        pst_x(point_index) = x(1);pst_x(point_index+1) = x(1);
+        pst_y(point_index) = y(1);pst_y(point_index+1) = y(1);
+    else
+        pst_x(point_index) = x(1);pst_y(point_index) = y(1);
+    end
+end
+end
